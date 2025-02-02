57005b9b9727ef5be6d67c6085a549fc4ac44683
nautbek <nautbek@gmail.com>
2025-02-02 14:04:15 +0300

    Add base delete logic.

--- src/components/Home/DistributionGroup.vue
+++ src/components/Home/DistributionGroup.vue
@@ -1,15 +1,48 @@
 <script setup>
-defineProps({
+import { defineProps } from 'vue';
+import axios from "axios";
+
+const props = defineProps({
   group: {
     name: String,
+    id: Number,
     required: true,
   },
 })
+
+const deleteGroup = async () => {
+  try {
+    // Получаем токен из localStorage
+    const token = localStorage.getItem('token');
+    const response = await axios.delete(`api/distributions/group`,
+        {
+          params: {
+            id: props.group.id,
+          },
+          headers: {
+            jwt_token: token
+          }
+        })
+    if (response.status === 200) {
+
+    }
+  } catch (err) {
+
+  }
+}
+
 </script>
 
 <template>
-  <H3>{{ group.name }}</H3><br>
+  <div class="group-item">
+    <H3>{{ group.name }}</H3><br>
+    <button class="delete-button" @click="deleteGroup">×</button>
+  </div>
 </template>
 
 <style scoped>
+.group-item {
+  display: flex;
+  justify-content: space-between;
+}
 </style>

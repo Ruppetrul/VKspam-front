dd8c110f0a541fd39600b3887d69481228d77f2c
nautbek <nautbek@gmail.com>
2025-02-02 18:47:38 +0300

    Router fix.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import {createRouter, createWebHistory} from "vue-router";
 import Login from "@/components/Login/Login.vue";
@@ -8,9 +8,9 @@ import Home from "@/components/Home/Home.vue";
 import DistributionGroupDetail from "@/components/DistributionGroupDetail/DistributionGroupDetail.vue";
 
 const routes = [
-    { path: '/', component: Login },
-    { path: '/home', name: 'home', component: Home },
-    { path: '/distribution-group/:id', component: DistributionGroupDetail }
+    {path: '/', component: Login},
+    {path: '/home', name: 'home', component: Home},
+    {path: '/distribution-group/:id', name: 'group-detail', component: DistributionGroupDetail}
 ]
 
 const router = createRouter({

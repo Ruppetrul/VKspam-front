9c8a018d2db9f08b7d5c1e95d1c3af6a739c246a
nautbek <nautbek@gmail.com>
2025-02-02 18:39:49 +0300

    Add detail route. Fix app style.

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import App from './App.vue'
 import {createRouter, createWebHistory} from "vue-router";
 import Login from "@/components/Login/Login.vue";
 import Home from "@/components/Home/Home.vue";
+import DistributionGroupDetail from "@/components/DistributionGroupDetail/DistributionGroupDetail.vue";
 
 const routes = [
     { path: '/', component: Login },
-    { path: '/home', name: 'home', component: Home }
+    { path: '/home', name: 'home', component: Home },
+    { path: '/distribution-group/:id', component: DistributionGroupDetail }
 ]
 
 const router = createRouter({

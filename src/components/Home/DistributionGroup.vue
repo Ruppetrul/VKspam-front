21a3a6cc57974c0a4ff16737379f0adc026e17d2
nautbek <nautbek@gmail.com>
2025-02-08 13:07:20 +0300

    Fix warning.

--- src/components/Home/DistributionGroup.vue
+++ src/components/Home/DistributionGroup.vue
@@ -17,7 +17,7 @@ const props = defineProps({
 
 <template>
   <div class="group-item">
-    <router-link :to="{ name: 'group-detail', params: {id: group.id} }"><H3>{{ group.name }}</H3><br></router-link>
+    <router-link :to="{ name: 'group-detail', params: {id: group.id} }"><h3>{{ group.name }}</h3><br></router-link>
     <button class="delete-button" @click="deleteGroup(group.id)">×</button>
   </div>
 </template>
